901f8410c7184d14a03750467fe24518e78a9287
Daniel Firth <locallycompact@gmail.com>
2020-06-06 14:29:21 +0000

    Simplify test script

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,8 +4,10 @@ import           Control.Comonad.Cofree
 import           Control.Comonad.Store
 import           Control.Comonad.Store.Zipper
 import           Control.Comonad.Zipper.Extra
+import           Control.Lens hiding ((:<))
 import           Data.Aeson
 import           Data.Aeson.With
+import           Data.Hashable.Time()
 import           Data.List.Split
 import           Data.Text.Time
 import           Development.Shake.Plus
@@ -80,7 +82,37 @@ rules = do
     allPosts <- postsC fp
     return $ genBlogNavbarData "Blog" "/posts/"
                   (T.pack . defaultPrettyMonthFormat)
-                  (defaultMonthUrlFragment) (HM.elems allPosts)
+                  defaultMonthUrlFragment (HM.elems allPosts)
+
+  postsZ <- newCache $ \fp -> do
+    xs <- sortedPosts fp
+    zs <- ifor xs $ \i (k, _) -> do
+      z <- zipper' (snd <$> xs)
+      return (k, seek i z)
+    return $ HM.fromList zs
+
+  blogIndexPageData <- newCache $ \fp -> do
+    xs <- sortedPosts fp
+    genIndexPageData (snd <$> xs) "Posts" ("/posts/pages/" <>) postsPerPage
+
+  blogTagIndexPageData <- newCache $ \fp -> do
+    xs <- sortedPosts fp
+    zs <- forM (viewAllPostTags (snd <$> xs)) $ \t -> do
+      let ys = filter (elem t . viewTags . snd) xs
+      k <- genIndexPageData (snd <$> ys) ("Posts tagged " <> t) (("/posts/tags/" <> t <> "/pages/") <>) postsPerPage
+      return (t, k)
+    return $ HM.fromList zs
+
+  blogMonthIndexPageData <- newCache $ \fp -> do
+    xs <- sortedPosts fp
+    zs <- forM (nub $ defaultMonthUrlFormat <$> viewAllPostTimes (snd <$> xs)) $ \t -> do
+      let t' = parseISODateTime . T.pack $ t
+      let ys = filter (sameMonth t' . viewPostTime . snd) xs
+      k <- genIndexPageData (snd <$> ys)
+                   (("Posts from " <>) . T.pack . defaultPrettyMonthFormat $ t')
+                   (("/posts/months/"  <> T.pack (defaultMonthUrlFormat t') <> "/pages/") <>) postsPerPage
+      return (t', k)
+    return $ HM.fromList zs
 
   let myPosts = ["posts/*.md"] `within` sourceFolder
 
@@ -95,26 +127,32 @@ rules = do
                . withRecentPosts rs $ v
         buildPageActionWithin (s' tmpl) v' out
 
+      myBuildBlogPage tmpl v out = do
+        k <- getBlogNavbar myPosts
+        myBuildPage tmpl (withJSON k v) out
+
+      myBuildPostListPage z out = do
+        let v = extract . extendPageNeighbours numPageNeighbours $ z
+        myBuildBlogPage $(mkRelFile "templates/post-list.html") v out
+
   o' "index.html" %^> \out -> do
-    src <- blinkAndMapM sourceFolder withMarkdownExtension $ out
+    src <- blinkAndMapM sourceFolder withMarkdownExtension out
     v   <- readMDC src
     myBuildPage $(mkRelFile "templates/index.html") v out
 
   o' "posts/*.html" %^> \out -> do
-    src <- blinkAndMapM sourceFolder withMarkdownExtension $ out
-    xs <- sortedPosts myPosts
-    let k = elemIndex src (fst <$> xs)
-    let z = fromJust $ liftA2 seek k $ zipper (snd <$> xs)
-    n   <- getBlogNavbar myPosts
-    let v' = withJSON n $ (extract z) 
-    myBuildPage $(mkRelFile "templates/post.html") v' out
+    src <- blinkAndMapM sourceFolder withMarkdownExtension out
+    xs <- postsZ myPosts
+    case HM.lookup src xs of
+      Nothing -> logError $ "Attempting to lookup non-existent post " <> display (WithinDisplay src)
+      Just x  -> myBuildBlogPage $(mkRelFile "templates/post.html") (extract x) out
 
   toc' <- mapM (mapM withHtmlExtension) $ fmap o' tableOfContents
-  void . sequence . flip extend toc' $ \xs -> (fmap toFilePath $ extract xs) %^> \out -> do
+  void . sequence . flip extend toc' $ \xs -> (toFilePath <$> extract xs) %^> \out -> do
     let getDoc = readMDC <=< blinkAndMapM sourceFolder withMarkdownExtension 
     ys <- mapM getDoc toc'
-    zs <- mapM getDoc $ (immediateShoots xs)
-    v  <- getDoc $ out
+    zs <- mapM getDoc (immediateShoots xs)
+    v  <- getDoc out
     let v' = withJSON (genTocNavbarData ys) . withSubsections zs $ v
     myBuildPage $(mkRelFile "templates/docs.html") v' out
 
@@ -122,12 +160,9 @@ rules = do
     copyFileChangedWithin (o' $(mkRelFile "posts/pages/1/index.html"))
 
   o' "posts/pages/*/index.html" %^> \out -> do
-    xs <- sortedPosts myPosts
     let n = (+ (-1)) . read . (!! 2) . splitOn "/" . toFilePath . extract $ out
-    p <- seek n <$> genIndexPageData (snd <$> xs) "Posts" ("/posts/pages/" <>) postsPerPage
-    k <- getBlogNavbar myPosts
-    let v = withJSON k $ extract $ extendPageNeighbours numPageNeighbours p
-    myBuildPage $(mkRelFile "templates/post-list.html") v out
+    xs <- blogIndexPageData myPosts
+    myBuildPostListPage (seek n xs) out
  
   o' "posts/tags/*/index.html" %^> \out -> do
     let t = (!! 2) . splitOn "/" . toFilePath . extract $ out
@@ -135,13 +170,12 @@ rules = do
     copyFileChangedWithin (o' i) out
 
   o' "posts/tags/*/pages/*/index.html" %^> \out -> do
-    let t = T.pack          . (!! 2) . splitOn "/" . toFilePath . extract $ out
-    xs <- filter (elem t . viewTags . snd) <$> sortedPosts myPosts
-    let n = (+ (-1)) . read . (!! 4) . splitOn "/" . toFilePath . extract $ out
-    p  <- seek n <$> genIndexPageData (snd <$> xs) ("Posts tagged " <> t) (("/posts/tags/"  <> t <> "/pages/") <>) postsPerPage
-    k <- getBlogNavbar myPosts
-    let v = withJSON k $ extract $ extendPageNeighbours numPageNeighbours p
-    myBuildPage $(mkRelFile "templates/post-list.html") v out
+     let t = T.pack          . (!! 2) . splitOn "/" . toFilePath . extract $ out
+     let n = (+ (-1)) . read . (!! 4) . splitOn "/" . toFilePath . extract $ out
+     xs <- blogTagIndexPageData myPosts
+     case HM.lookup t xs of
+       Nothing -> logError $ "Attempting to lookup non-existant tag " <> display t
+       Just x  -> myBuildPostListPage (seek n x) out
 
   o' "posts/months/*/index.html" %^> \out -> do
     let t = (!! 2) . splitOn "/" . toFilePath . extract $ out
@@ -149,52 +183,48 @@ rules = do
     copyFileChangedWithin (o' i) out
 
   o' "posts/months/*/pages/*/index.html" %^> \out -> do
-    let t = parseISODateTime . T.pack . (!! 2) . splitOn "/" . toFilePath . extract $ out
-    xs <- filter (sameMonth t . viewPostTime . snd) <$> sortedPosts myPosts
-    let n = (+ (-1)) . read  . (!! 4) . splitOn "/" . toFilePath . extract $ out
-    p  <- seek n <$> genIndexPageData (snd <$> xs)
-                       (("Posts from " <>) . T.pack . defaultPrettyMonthFormat $ t)
-                       (("/posts/months/"  <> T.pack (defaultMonthUrlFormat t) <> "/pages/") <>) postsPerPage
-    k <- getBlogNavbar myPosts
-    let v = withJSON k $ extract $ extendPageNeighbours numPageNeighbours p
-    myBuildPage $(mkRelFile "templates/post-list.html") v out
+     let t = parseISODateTime . T.pack . (!! 2) . splitOn "/" . toFilePath . extract $ out
+     let n = (+ (-1)) . read  . (!! 4) . splitOn "/" . toFilePath . extract $ out
+     xs <- blogMonthIndexPageData myPosts
+     case HM.lookup t xs of
+       Nothing -> logError $ "Attempting to lookup non-existant month " <> displayShow t
+       Just x  -> myBuildPostListPage (seek n x) out
 
-  phony "index" $
-    needIn outputFolder [$(mkRelFile "index.html")]
+  o' ["css//*", "js//*", "webfonts//*", "images//*"] |%^> \out ->
+    copyFileChangedWithin (blinkLocalDir sourceFolder out) out
+
+  let simplePipeline f = getDirectoryFiles sourceFolder >=> mapM f >=> needIn outputFolder
+      verbatimPipeline = simplePipeline return
+
+  phony "statics" $ verbatimPipeline ["css//*", "js//*", "webfonts//*", "images//*"]
+
+  phony "index" $ needIn outputFolder [$(mkRelFile "index.html")]
 
   phony "post-index" $ do
-    xs <- sortedPosts myPosts
-    ps <- paginate' postsPerPage xs
-    fs <- defaultPagePaths [1..size ps]
-    needIn (outputFolder </> $(mkRelDir "posts")) ($(mkRelFile "index.html") : fs)
+     ps <- blogIndexPageData myPosts
+     fs <- defaultPagePaths [1..size ps]
+     needIn (outputFolder </> $(mkRelDir "posts")) ($(mkRelFile "index.html") : fs)
 
   phony "by-tag-index" $ do
-    xs <- sortedPosts myPosts
-    forM_ (viewAllPostTags (snd <$> xs)) $ \t -> do
-      u <- parseRelDir $ T.unpack t
-      let xs' = filter (elem t . viewTags . snd) xs
-      ps <- paginate' postsPerPage xs'
-      fs <- defaultPagePaths [1..size ps]
-      let tagFolder = outputFolder </> $(mkRelDir "posts/tags") </> u
-      needIn tagFolder ($(mkRelFile "index.html") : fs)
+     ps <- blogTagIndexPageData myPosts
+     void $ flip HM.traverseWithKey ps $ \t z -> do
+       u  <- parseRelDir $ T.unpack t
+       fs <- defaultPagePaths [1..size z]
+       let tagFolder = outputFolder </> $(mkRelDir "posts/tags") </> u
+       needIn tagFolder ($(mkRelFile "index.html") : fs)
 
   phony "by-month-index" $ do
-    xs <- sortedPosts myPosts
-    forM_ (viewAllPostTimes (snd <$> xs)) $ \t -> do
-      u <- parseRelDir $ defaultMonthUrlFormat t
-      let monthFolder = outputFolder </> $(mkRelDir "posts/months") </> u
-      let xs' = filter (sameMonth t . viewPostTime . snd) xs
-      ps <- paginate' postsPerPage xs'
-      fs <- defaultPagePaths [1..size ps]
-      needIn monthFolder ($(mkRelFile "index.html") : fs)
+     ps <- blogMonthIndexPageData myPosts
+     void $ flip HM.traverseWithKey ps $ \t z -> do
+       u  <- parseRelDir $ defaultMonthUrlFormat t
+       fs <- defaultPagePaths [1..size z]
+       let monthFolder = outputFolder </> $(mkRelDir "posts/months") </> u
+       needIn monthFolder ($(mkRelFile "index.html") : fs)
 
   phony "docs" $
     mapM withHtmlExtension tableOfContents >>= needIn outputFolder
-    
-  phony "posts" $
-    getDirectoryFilesWithin' (s' ["posts/*.md"]) >>= 
-      mapM (blinkAndMapM outputFolder withHtmlExtension) >>=
-        needWithin'
+
+  phony "posts" $ simplePipeline withHtmlExtension ["posts/*.md"]
 
   phony "clean" $ do
     logInfo $ "Cleaning files in " <> display (PathDisplay outputFolder)
@@ -205,7 +235,7 @@ tests xs = testGroup "Rendering Tests" $
   map ( \x -> goldenVsFile x x
      (replace "golden" "public" x)
      (return ())) xs
-  where replace from to' = intercalate to' . splitOn from
+  where replace fr to' = intercalate to' . splitOn fr
 
 main :: IO ()
 main = do
