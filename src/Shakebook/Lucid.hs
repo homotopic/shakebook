3f426a2c4787831362eead67836d0a0af5dac39a
Dan Firth <locallycompact@gmail.com>
2020-07-28 00:13:51 +0100

    Add lucid

---
+++ src/Shakebook/Lucid.hs
@@ -0,0 +1,16 @@
+module Shakebook.Lucid where
+
+import Data.Binary
+import Development.Shake.Plus
+import Lucid
+import RIO
+
+instance Binary (Html ()) where
+  put = put . renderText
+  get = fmap toHtmlRaw (get :: Get Text)
+
+instance Hashable (Html ()) where
+  hashWithSalt n x = n `hashWithSalt` renderText x
+
+instance Eq (Html ()) where
+  a == b = renderText a == renderText b
