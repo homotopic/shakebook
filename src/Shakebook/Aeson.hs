808b0445eec88358b00a791d08b64ed0b9195b72
Dan Firth <locallycompact@gmail.com>
2020-09-19 12:24:52 +0100

    Move functions to composite-aeson-writeonly and composite-aeson-cofree-list

--- src/Shakebook/Aeson.hs
+++ src/Shakebook/Aeson.hs
@@ -1,32 +1,21 @@
-module Shakebook.Aeson where
+module Shakebook.Aeson (
+  htmlJsonFormat
+, styleJsonFormat
+, cofreeListJsonFormat
+, writeOnlyJsonFormat
+, noRead
+, WriteOnlyJsonField
+) where
 
 import           Composite.Aeson
-import           Control.Comonad.Cofree
-import           Data.Aeson as A
-import           Data.Aeson.BetterErrors
+import           Composite.Aeson.Format.CofreeList
+import           Composite.Aeson.WriteOnly
+import Data.Aeson
 import           RIO
-import qualified RIO.Vector as V
 import           Shakebook.Lucid
-
-data WriteOnlyJsonField = WriteOnlyJsonField
-  deriving Show
-
-noRead :: ParseT WriteOnlyJsonField Identity a
-noRead = throwCustomError WriteOnlyJsonField
-
-writeOnlyJsonFormat :: (a -> Value) -> JsonFormat e a
-writeOnlyJsonFormat f = jsonFormatWithoutCustomError $ JsonFormat $ JsonProfunctor f noRead
 
 htmlJsonFormat :: JsonFormat e HtmlFragment
 htmlJsonFormat = writeOnlyJsonFormat $ String . unHtmlFragment
 
 styleJsonFormat :: JsonFormat e StyleFragment
 styleJsonFormat = writeOnlyJsonFormat $ String . unStyleFragment
-
-cofreeListJsonFormat :: JsonFormat e a -> JsonFormat e (Cofree [] a)
-cofreeListJsonFormat f = writeOnlyJsonFormat p where
-                           p = \(x :< xs) -> object $ ["head" A..= toJsonWithFormat f x] <> (
-                                                  case xs of
-                                                    [] -> []
-                                                    _  -> ["tail" A..= Array (V.fromList $ p <$> xs) ]
-                                                   )
